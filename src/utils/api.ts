b8aec43a4b95f51afeebc5ec6533dda9c8ac0e8a
luanesouza <annie.usa25@gmail.com>
2023-07-11 21:48:43 -0400

    chore: api call to rails backend

---
+++ src/utils/api.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosResponse } from 'axios'
+import { User } from '../definitions';
+
+const BASE_URL = import.meta.env.VITE_BASE_URL ?? 'http://localhost:3000'
+
+export async function getAllUsers(): Promise<User[]| undefined> {
+    try {
+        const response = await axios(`${BASE_URL}/users`)
+        return response.data;
+    } catch (e: unknown) {
+        console.error(e);
+        return;
+    }
+}
+
+export async function postProfileImage(userId: number, imageBase64: File | ArrayBuffer | null): Promise<User | undefined> {
+    try {
+        const response = await axios.put(`${BASE_URL}/users/${userId}`, {user: {avatar: imageBase64}})
+        return response.data;
+    } catch (e: unknown) {
+        console.error(e);
+    }
+}
+
+export async function createAConnection(userId: number) {
+    try {
+        const response = await axios.put(`${BASE_URL}/users/${userId}/connections/create`)
+        return response.data;
+    } catch (e: unknown) {
+        console.error(e);
+        return;
+    }
+}
