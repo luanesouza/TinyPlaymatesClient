cc1de20d415ab0c743d53136a2ece2d0cfb4d8b3
luanesouza <annie.usa25@gmail.com>
2023-07-13 20:09:12 -0400

    lint: adds eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,22 @@
 module.exports = {
-    env: { browser: true, es2020: true },
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react-hooks/recommended",
-        "prettier",
-    ],
-    parser: "@typescript-eslint/parser",
-    parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-    plugins: ["react-refresh"],
-    rules: {
-        "react-refresh/only-export-components": "warn",
-        indent: "warn",
-    },
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    quotes: ['error', 'single'],
+    // we want to force semicolons
+    semi: ['error', 'always'],
+    // we use 2 spaces to indent our code
+    indent: ['error', 2],
+    // we want to avoid extraneous spaces
+    'no-multi-spaces': ['error'],
+  },
 };
