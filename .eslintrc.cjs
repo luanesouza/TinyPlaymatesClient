49b3154cd4b8dbe4208d10c5b81cba48db657ea9
Annie Souza <luanesouza411@gmail.com>
2023-07-19 14:25:04 -0400

    eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,22 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'prettier',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-    quotes: ['error', 'single'],
-    // we want to force semicolons
-    semi: ['error', 'always'],
-    // we use 2 spaces to indent our code
-    indent: ['error', 2],
-    // we want to avoid extraneous spaces
-    'no-multi-spaces': ['error'],
-  },
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+		'prettier',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+	plugins: ['react-refresh'],
+	rules: {
+		'react-refresh/only-export-components': 'warn',
+		quotes: ['error', 'single'],
+		// we want to force semicolons
+		semi: ['error', 'always'],
+		// we want to avoid extraneous spaces
+		'no-multi-spaces': ['error'],
+		indent: 'off',
+	},
 };
